da5f4312f0c5ec22e21c88c213d0444deba3f5c9
Brian Weinthal <76851117+189427@users.noreply.github.com>
2022-05-22 16:40:33 -0400

    fixed bug preventing fighting the next robot

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,9 +4,6 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
@@ -14,9 +11,8 @@ var enemyAttack = 12;
 // fight function
 var fight = function(enemyName) {
 
+  while(enemyHealth > 0) {
 
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -67,9 +63,12 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+}
 }; // end of fight function
 
   // run fight function to start game
-  for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-  }
+  for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+}
